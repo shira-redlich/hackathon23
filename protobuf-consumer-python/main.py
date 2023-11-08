cb9ca2e5a5c8f41615268d61e6a7b9da45db278a
Shira Gabai <Shira_Gabai@Dell.com>
2023-11-08 15:44:09 +0200

    Add java and go projects

--- protobuf-consumer-python/main.py
+++ protobuf-consumer-python/main.py
@@ -1,1 +1,13 @@
-print("Hackathon 2023! protobuf PYTHON consumer")
+# main.py
+
+import event_pb2
+
+def main():
+    event = event_pb2.event()
+
+
+    # Print the model
+    print(event)
+
+if __name__ == "__main__":
+    main()
